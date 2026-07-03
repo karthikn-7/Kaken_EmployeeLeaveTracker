490bdc460772de177555c8e62bcbabd7974ba2a2
karthikn-7 <karthikkat7777@gmail.com>
2026-07-03 13:14:51 +0530

    initial commit spring boot employee leave tracker

---
+++ src/test/java/com/kaken/EmployeeLeaveTrackerSpring/EmployeeLeaveTrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaken.EmployeeLeaveTrackerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLeaveTrackerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
